b28804834ed2a4475b99aa25e38943aaa2445083
Ethan Tuttle <ethanctuttle@gmail.com>
2025-06-28 12:33:18 -0400

    Added line to Hello World

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -2,5 +2,6 @@ package ver1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		System.out.println("Ethan is working locally on his branch");
 	}
 }
